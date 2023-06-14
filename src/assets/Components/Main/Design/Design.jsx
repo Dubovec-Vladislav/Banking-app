c0f3d30b61bc65119794aa180e72ee4c2e8898a8
Dubovec-Vladislav <dubovecvladislav0@gmail.com>
2023-06-14 16:34:08 +0300

    Creating design block

---
+++ src/assets/Components/Main/Design/Design.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import style from './Design.module.scss'
+import Button from '../General/Button'
+
+function Design(props) {
+  return (
+    <div className={style.left}>
+      <div className={style.body}>
+        <div className={style.left}>
+          <div className={style.img}></div>
+          <div className={style.title}>Стандартный дизайн</div>
+          <div className={style.text}> Создайте в конструкторе собственный дизайн карты за 3 шага. 7 роскошных текстур металла, десятки стильных оформлений и шрифтов</div>
+          <div className={style.btn}><Button text={"Заказать за 1790₽"} /></div>
+        </div>
+        <div className={style.right}>
+          <div className={style.img}></div>
+          <div className={style.title}>Индивидуальный дизайн</div>
+          <div className={style.text}>Персональная разработка карты под ваши запросы. Эксклюзивные дизайнерские решения только для вас.</div>
+          <Button text={"Заказать за 4990₽"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Design;
--- src/assets/Components/Main/Main.jsx
+++ src/assets/Components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Main.scss'
 import Hello from './Hello/Hello'
+import Design from './Design/Design'
 
 function Main(props) {
   return (
     <div className="main">
       <Hello />
+      <Design />
     </div>
   );
 };
